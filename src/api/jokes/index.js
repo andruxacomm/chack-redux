a7d193e33ed25351258215f240467178469f0c7c
andrewglotov <a.glotov@timepad.ru>
2021-04-02 18:11:32 +0500

    fixes

--- src/api/jokes/index.js
+++ src/api/jokes/index.js
@@ -1,40 +1,21 @@
 import axios from "axios";
 
 export const getRandom = async () => {
-    try {
-        const {data} = await axios.get('https://api.chucknorris.io/jokes/random');
-        if (!data.id) throw new Error('invalid Joke id');
-        return data;
-    } catch (e) {
-        throw new Error(e);
-    }
+    const {data} = await axios.get('https://api.chucknorris.io/jokes/random');
+    return data;
 }
 
 export const getRandomByCategory = async (id) => {
-    try {
-        const {data} = await axios.get('https://api.chucknorris.io/jokes/random?category='+ id);
-        if (!(data?.categories || []).includes(id)) throw new Error('invalid Joke id');
-       return data;
-    } catch (e) {
-        throw new Error(e);
-    }
+    const {data} = await axios.get('https://api.chucknorris.io/jokes/random?category='+ id)
+    return data;
 }
 
 export const getCategories = async () => {
-    try {
-        const {data} = await axios.get('https://api.chucknorris.io/jokes/categories');
-        if (!Array.isArray(data)) throw new Error('data is not a array');
-        return data;
-    } catch (e) {
-        throw new Error(e);
-    }
+    const {data} = await axios.get('https://api.chucknorris.io/jokes/categories');
+    return data;
 }
 
 export const search = async (string) => {
-    try {
-        const {data} = await axios.get('https://api.chucknorris.io/jokes/search?query=' + string)
-        return data;
-    } catch (e) {
-        throw new Error(e);
-    }
+    const {data} = await axios.get('https://api.chucknorris.io/jokes/search?query=' + string)
+    return data;
 }
